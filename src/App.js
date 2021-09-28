aa35600f5349c303cad3b43f90439ca89e1e97cc
1-Mirage <prasad.ashish162@gmail.com>
2021-09-28 22:22:49 +0530

    Uploading necessary files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "🍉": "Watermelon",
+  "🍌": "Banana",
+  "🥭": "Mango",
+  "🍓": "Strawberry",
+  "🥦": "Broccoli",
+  "🥞": "Pancake"
+};
+
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>inside outttttt</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Search your emoji"}
+      />
+      <h2> {emoji} </h2> {}
+      <h3> {meaning} </h3> {}
+      {emojis.map((emoji) => (
+        <span onClick={() => emojiClickHandler(emoji)}> {emoji} </span>
+      ))}
+    </div>
+  );
+}
